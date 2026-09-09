b9239dcfb62d905561aba9d4a848d34e18f66982
Mubai <1903617153@qq.com>
2026-09-09 17:44:50 +0800

    处理重复添加员工的异常

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -3,8 +3,11 @@ package com.sky.handler;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,17 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException sqlEx){
+        // Duplicate entry 'admin' for key 'employee.idx_username'
+        String message = sqlEx.getMessage();
+        if(message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            String username = split[2];
+            return Result.error(username + "已存在");
+        }
+
+        log.error("异常信息：{}", sqlEx.getMessage());
+        return Result.error("未知异常");
+    }
 }
